19919fbbcbf0135c960faf1090ad2072665abf42
chaseparkeratx <112991010+chaseparkeratx@users.noreply.github.com>
2023-07-17 19:58:13 -0500

    final product

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 44
+
+app.use(express())
 
 app.get('/', (req, res) => res.send('default route'))
 
+
 app.listen(port, () => {
-  console.log('app is listening on:', port)
+  console.log('app is listening on port:', port)
 })
+
